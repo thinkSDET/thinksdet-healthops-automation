92bf86709313ba5bcf307aec7a69777a6b5960c1
Qa-Automation30 <qaautomation30@gmail.com>
2026-09-24 16:12:14 +0530

    Add OrganizationData interface and factory for dynamic test data

--- src/utils/OrganizationDataFactory.ts
+++ src/utils/OrganizationDataFactory.ts
@@ -1,9 +1,10 @@
 import testData from '../test-data/organization-registration.json'
+import { OrganizationData } from '../types/OrganizationData';
 import { TestDataGenerator } from './TestDataGenerator';
 
 export class OrganizationDataFactory {
 
-    static validOrganization() {
+    static validOrganization() : OrganizationData  {
         return {
             ...testData.validOrganization,
             organizationName: TestDataGenerator.generateOrganizationName(),
